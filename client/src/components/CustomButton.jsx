c904d345a1d6e52383059a94d71bda047c77ae3e
Pua-Sangjun <daniellyu99@ailo.kr>
2023-07-19 02:36:04 +0900

    added styling for outline button

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import state from "../store";
 import { useSnapshot } from "valtio";
+import { getContrastingColor } from "../config/helpers";
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
@@ -8,7 +9,13 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
     if (type === "filled") {
       return {
         backgroundColor: snap.color,
-        color: "#FFF",
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === "outline") {
+      return {
+        borderWidth: "1px",
+        borderColor: snap.color,
+        color: snap.color,
       };
     }
   };
@@ -16,6 +23,7 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
       style={generateStyle(type)}
+      onClick={handleClick}
     >
       {title}
     </button>
